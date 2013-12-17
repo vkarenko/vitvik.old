151ae2a4cfd6683db9b687547f9c83ddc14bf8ed
Vitalii <vitalii.karenko@me.com>
2013-12-17 15:59:25 +0200

    Welcome page some optimizations

--- js/base.js
+++ js/base.js
@@ -3,7 +3,7 @@
 	// Document ready function
 	// =================================================
 	$(function() {
-		//Query Loader
+		// Query Loader
 		$('.html5 body').queryLoader2({
 			barHeight: 1,
 			barColor: '#E5D9D0',
@@ -12,7 +12,50 @@
 			completeAnimation: 'grow'
 		});
 
-		//bubles animations
+		// =================================================
+		// sizes of main elements and actions with logo
+		// =================================================
+		var WP = $('.html5 .welcome-page'),
+			LC = $('.html5 .vitalii');
+
+		// resize WelcomePage
+		function resize_WP() {
+			WP.height($(window).height() - 16);
+		};
+		resize_WP();
+
+		// resize LeftColumn
+		function resize_LC() {
+			if ($(window).height() > 919 && $(window).width() > 760) {
+				LC.height($(window).height())
+			};
+		};
+		resize_LC();
+
+		// Window resize
+		$(window).resize(function() {
+			resize_WP();
+			resize_LC();
+		});
+
+		//events with Welcome logo
+		var WR = $('.html5 .wrapper'),
+			WP_WL = $('.html5 .welcome-logo-container');
+
+		WP_WL.click(function() {
+			WP.slideUp('800');
+			WR.delay(1000).fadeIn('800');
+		});
+
+		WP_WL.hover(function() {
+			$('body').addClass('hovered')
+		}, function() {
+			$('body').removeClass('hovered');
+		});
+
+		// =================================================
+		// Animations with bubles
+		// =================================================
 		var $bubles_1 = $('.html5 .bubles-one'),
 			$bubles_2 = $('.html5 .bubles-two'),
 			$bubles_3 = $('.html5 .bubles-three');
@@ -20,6 +63,7 @@
 		$bubles_1.delay(1000).fadeIn(3000);
 		$bubles_2.delay(1000).fadeIn(2000);
 		$bubles_3.delay(1000).fadeIn(1000);
+
 		//bubles move
 		function movebublesone() {
 			$bubles_1
@@ -31,7 +75,6 @@
 					y: 0,
 					x: 0
 				}, 11000, movebublesone);
-
 		}
 		movebublesone();
 
@@ -45,7 +88,6 @@
 					y: 0,
 					x: 0
 				}, 10000, movebublestwo);
-
 		}
 		movebublestwo();
 
@@ -59,38 +101,12 @@
 					y: 0,
 					x: 0
 				}, 9000, movebublesthree);
-
 		}
 		movebublesthree();
 
-		//Welcome page animations
-		$(".html5 .welcome-page").height($(window).height() - 16);
-		$(window).resize(function() {
-			$(".html5 .welcome-page").height($(window).height() - 16);
-		});
-
-		//highscreens hack
-		if ($(window).height() > 950 && $(window).width() > 1025) {
-			$(".vitalii").height($(document).height());
-		};
-		$(window).resize(function() {
-			if ($(window).height() > 950 && $(window).width() > 1025) {
-				$(".vitalii").height($(document).height());
-			};
-		});
-
-		//events with Welcome logo
-		$(".html5 .welcome-logo-container").click(function() {
-			$(".html5 .welcome-page").slideUp("800");
-			$(".html5 .wrapper").delay(1000).fadeIn("800");
-		});
-		$(".html5 .welcome-logo-container").hover(function() {
-			$('body').addClass('hovered')
-		}, function() {
-			$('body').removeClass('hovered');
-		});
-
-		//arrows moving
+		// =================================================
+		// arrows moving
+		// =================================================
 		function movearrows() {
 			var $vitarr = $('.html5 .vitalii a.arrow');
 
